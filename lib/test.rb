a619e9f32500d62a39330dd23d6a5b738891b39a
Kingdon Barrett <kingdon@weave.works>
2023-04-24 19:38:02 -0400

    wast again - this time passing pointer to a string
    I was so wrong about where this was going
    
    I thought I'd be receiving an http client in the mail, turns out we're
    going to be passing pointers around and it's not like that at all...
    
    The `test.rb` is updated so there is no longer any expectation that we
    take our http client as a parameter. We'll actually be exporting ours,
    so the Rust/Wasm HTML parsing library that we've built can receive the
    HTML as a pointer in memory. (That way we don't need to copy ... ?)
    
    This wast at least works, we receive the "Hello World" string and the
    Ruby program accepts it unquestioningly as a number to return inside of
    the Ruby hash expressed as a to_string output:
    
    {:time=>2023-04-24 19:41:40.018884 -0400, :count=>"Hello, World!"}
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- lib/test.rb
+++ lib/test.rb
@@ -2,6 +2,15 @@ require 'gammo'
 require 'open-uri'
 require 'wasmer'
 
+# prelude.rb
+class AssertionError < RuntimeError
+end
+
+def assert &block
+  raise AssertionError unless yield
+end
+# /prelude
+
 BIG_HONKIN_SELECTOR = "#repo-content-turbo-frame > div > div > div > div.d-flex.flex-column.flex-md-row.mt-n1.mt-2.gutter-condensed.gutter-lg.flex-column > div.col-12.col-md-3.flex-shrink-0 > div:nth-child(3) > div.container-lg.my-3.d-flex.clearfix > div.lh-condensed.d-flex.flex-column.flex-items-baseline.pr-1".freeze
 
 def wasmer_current_download_count(html)
@@ -15,38 +24,50 @@ def wasmer_current_download_count(html)
   file = File.expand_path "stat.wasm", File.dirname(__FILE__)
   # wasm_bytes = IO.read(file, mode: "rb")
   wasm_bytes = Wasmer::wat2wasm(
-    (<<~WAST)
-    (module
-      (type $sum_t (func (result i32)))
-      (func $sum_f (type $sum_t) (result i32)
-        i32.const 1
-        i32.const 2
-        i32.add)
-      (export "count_from_html" (func $sum_f)))
-    WAST
-  )
+  (<<~WAST)
+  (module
+    (type $hello_t (func (result i32)))
+    (func $hello (type $hello_t) (result i32)
+        i32.const 42)
+    (memory $memory 1)
+    (export "hello" (func $hello))
+    (export "mem" (memory $memory))
+    (data (i32.const 42) "Hello, World!"))
+  WAST
+)
+
 
   # Wasmer setup stuff
   store = Wasmer::Store.new
   module_ = Wasmer::Module.new store, wasm_bytes
-  # wasi_version = Wasmer::Wasi::get_version module_, true
 
   # Setup the wasm module with some system parameters
-  # wasi_env =
-  #   Wasmer::Wasi::StateBuilder.new('stats')
-  #     .argument('html/content')
-  #     .map_directory('html', cache)
-  #     .finalize
-  # import_object = wasi_env.generate_import_object store, wasi_version
+#   import_object = Wasmer::ImportObject.new
+#   sum_host_function = Wasmer::Function.new(
+#     store,
+#     method(:html),
+#   #                         x                  y                    result
+#   Wasmer::FunctionType.new([Wasmer::Type::I32, Wasmer::Type::I32], [Wasmer::Type::I32])
+# )
 
   # Call the Wasm (it may use the system interface for IO)
   instance = Wasmer::Instance.new module_, nil
-  # Let's not call main...
-  # instance.exports._start.()
-  # results = instance.exports.count_from_html.()
-  results = instance.exports.count_from_html.()
+  pointer = instance.exports.hello.()
+  assert { pointer == 42 }
 
-  return results
+  memory = instance.exports.mem
+  assert { memory.is_a?(Wasmer::Memory) }
+  reader = memory.uint8_view pointer
+  returned_string = reader.take(13).pack("C*").force_encoding('utf-8')
+  assert { returned_string == 'Hello, World!' }
+
+  # results = instance.exports.count_from_html.()
+
+  return returned_string
+end
+
+def html
+  http_client_read
 end
 
 def gammo_current_download_count(html)
@@ -66,16 +87,24 @@ def noko_current_download_count(html)
   v = u[3].attributes["title"].value
 end
 
-def get_current_stat_with_time(http_client)
+def get_current_stat_with_time
+  client = Proc.new do |url|
+    URI.open(url)
+  end
+
   t = Time.now
-  h = http_client.call('https://github.com/fluxcd/flagger/pkgs/container/flagger')
+  h = http_client_wrapped(client)
   c = wasmer_current_download_count(h)
 
   {time: t, count: c}
 end
 
-client = Proc.new do |url|
-  URI.open(url)
+def http_client_wrapped(http_client)
+  http_client.call('https://github.com/fluxcd/flagger/pkgs/container/flagger')
 end
 
-puts get_current_stat_with_time(client)
+def http_client_read
+  http_client_wrapped.read
+end
+
+puts get_current_stat_with_time
